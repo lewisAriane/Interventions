98c4e0cc08e0b3d49cf365b66104df8a351577db
ARIANE LEWIS <lewis.ariane@gmail.com>
2023-03-10 15:13:58 -0500

    Fin du tp6

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Inter-acceuil',
+  templateUrl: './acceuil.component.html',
+  styleUrls: ['./acceuil.component.css']
+})
+export class AcceuilComponent {
+
+}
